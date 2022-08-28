217bc20f20a78911f130c33b34ac2edf145d0ed9
Drew Hund <languageinvestigator@gmail.com>
2022-08-28 14:04:49 -0400

    tabs and categories set

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,10 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { sanityClient } from '../../sanity'
+import { groq } from 'next-sanity'
+
+const query = groq`*[_type == "category"] {
+  _id,
+  ...
+}`;
 
 
 type Data = {
   categories: Category[]
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // const categories = await sanityClient.fetch(query)
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const categories = await sanityClient.fetch(query);
+
+  console.log(categories);
+
+  res.status(200).json({ categories })
 }
